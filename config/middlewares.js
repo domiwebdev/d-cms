8e6584bc2381f7cc94a042d33479e7fa6622f139
Daniel Brinca <brinca@gmail.com>
2023-12-30 20:13:30 +0000

    Change upload provider to AWS S3

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com', 'domi-cms.s3.eu-west-1.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com', 'domi-cms.s3.eu-west-1.amazonaws.com'],
           upgradeInsecureRequests: null,
         },
       },
